c84b66168816bfc6c81799db5f62db0721c35f3e
varunallampalli18 <varunallampalli18@gmail.com>
2026-07-09 18:16:40 +0530

    Time: 26 ms (29.38%), Space: 25.9 MB (30.47%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        dub=set()
+        for i in nums:
+            if i in dub:
+                return True
+            dub.add(i)
+        return False
+
+
+
+
+        
